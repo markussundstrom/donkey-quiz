af61c33149231f44c506b6e0b54f4ef8b2cc4189
Markus Sundström <sundstrom.markus@gmail.com>
2022-10-17 10:51:37 +0200

    finished questions API

--- app/Http/Controllers/QuizData.php
+++ app/Http/Controllers/QuizData.php
@@ -7,10 +7,29 @@ use Illuminate\Http\Request;
 class QuizData extends Controller
 {
     function provideQuestions() {
-        $questionlib = json_decode(file_get_contents(storage_path('/') . '/questions.json'));
+        $questionlib = json_decode(file_get_contents(storage_path('/') . '/questions.json'), true);
+
+        $qlibcount = count($questionlib);
         $questions = array();
-        for ($i = 0; $i < 5; $i++) {
-            array_push($questions, $questionlib[$i]);
+        $used_questions = array();
+        $categories = array('Film & TV' => 0,
+                            'Geografi' => 0,
+                            'Historia' => 0,
+                            'Musik' => 0,
+                            'Övrigt' => 0,
+                            'Vetenskap' => 0,
+                            'Sport' => 0);
+
+        while (count($questions) < 35) {
+            $randomnum = random_int(0, $qlibcount - 1);
+            if ($categories[$questionlib[$randomnum]['category']] >= 5) {
+                continue;
+            }
+            if (in_array($randomnum, $used_questions, true)) {
+                continue;
+            }
+            array_push($questions, $questionlib[$randomnum]);
+            $categories[$questionlib[$randomnum]['category']]++;
         }
         return response()->json($questions);
     }
